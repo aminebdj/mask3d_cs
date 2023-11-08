fb1818b7dc46ba84fc0a0159bbfd7bf6dab35dca
Mohamed El Amine Boudjoghra <95423980+aminebdj@users.noreply.github.com>
2023-11-08 18:36:49 +0400

    .

---
+++ scripts/train_oracle/C/train_t1.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+export OMP_NUM_THREADS=3  # speeds up MinkowskiEngine
+
+# # TRAIN
+python main_instance_segmentation.py \
+general.OW_task="task1" \
+general.split="C" \
+general.experiment_name="Mask3D_C_t1" \
+general.project_name="3D_OWIS_closed_setting" \
+general.train_oracle=True
